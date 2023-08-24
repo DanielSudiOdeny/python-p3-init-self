1b557c4123c1291810841fea47bf1b8e1f4a575c
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-08-24 19:22:55 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+
+fido = Dog("Fido")
+snoopy = Dog("Snoopy")
+
+
+snowy = Dog("Snowy", "Tennis Ball")
+print(snowy.breed)
+
+fido.get_adopted("Daniel")
+print(fido.owner)
+fido.owner
